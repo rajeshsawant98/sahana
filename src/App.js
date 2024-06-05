a21ff2e221fb159896885a0e62c3f9adf1fbc725
Sawant <12312@intelizign.com>
2024-06-05 12:47:04 +0530

    added sso using google identity services

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode";
+import "./App.css";
 
 function App() {
+  const [user, setUser] = useState({});
+
+  function handleCallbackResponse(response) {
+    console.log("Encoded JWT ID Token: " + response.credential);
+    var userObject = jwtDecode(response.credential);
+    console.log(userObject);
+    setUser(userObject);
+    document.getElementById("signInDiv").hidden = true;
+  }
+
+  function handleSignOut(event) {
+    setUser({});
+    document.getElementById("signInDiv").hidden = false;
+  }
+
+  useEffect(() => {
+    /*global google*/
+    google.accounts.id.initialize({
+      client_id:
+        "505025857168-olhtcsvr2pmpu84k0gb25rkh61qksbm8.apps.googleusercontent.com",
+      callback: handleCallbackResponse,
+    });
+
+    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+      theme: "outline",
+      size: "large",
+    });
+
+    google.accounts.id.prompt();
+  }, []);
+  // If we have no user: sign in button
+  // If we have a user: show log out button
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="signInDiv"></div>
+      {user && (
+        <div>
+          <img src={user.picture}></img>
+          <h3>{user.name}</h3>
+        </div>
+      )}
+      {
+        Object.keys(user).length != 0 && 
+        <button onClick={(e) => handleSignOut(e)}>Sign Out</button>}
     </div>
   );
 }
